26dfe3aef27d9259aff4c7390aade9c6afd88ebd
HostileGenius <135417455+HostileGenius@users.noreply.github.com>
2025-01-27 16:44:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = FastAPI()
 @app.get("/data")
 def get_csv():
     # Replace 'data.csv' with your actual CSV file path
-    return FileResponse("data.csv", media_type="text/csv", filename="C:/Users/shiva/Downloads/Cleaned_Indian_Food_Dataset/Recipes.csv")
+    return FileResponse("data.csv", media_type="text/csv", filename="https://github.com/HostileGenius/recipes/Recipes.csv")
