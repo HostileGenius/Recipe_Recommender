775b5b2dc8b071f498a4012005c90f39d6d48df1
HostileGenius <135417455+HostileGenius@users.noreply.github.com>
2025-01-27 16:16:50 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
-
-app = FastAPI()
-
-@app.get("/data")
-def get_csv():
-    # Replace 'data.csv' with your actual CSV file path
-    return FileResponse("data.csv", media_type="text/csv", filename="data.csv")
